194569564c85f4c101be62f1c03c824cf09277e0
Sam Chrisinger <s.chrisinger@gmail.com>
2015-08-11 14:59:04 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# About this repository
+
+This repo is meant to be a collection of common licenses made easily accessable as javascript variables.
+
+## Naming conventions
+
+**Object Keys**
+```({common abbreviation})({version})(_{minor version})*```
+
+**File Names**
+```({lower-case common abbreviation})-({version})(.{minor version})*```
---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+    MIT: {
+        name: 'MIT License',
+        text: require('licenses/mit.txt')
+    },
+    GPL2: {
+        name: 'GNU General Public License (GPL) 2.0',
+        text: require('licenses/gpl-2.0.txt')
+    },
+    GPL3: {
+        name: 'GNU General Public License (GPL) 3.0',
+        text: require('licenses/gpl-3.0.txt')
+    },
+    Apache2: {
+        name: 'Apache License 2.0',
+        text: require('licenses/apache-2.0.txt')
+    },
+    BSD2: {
+        name: 'BSD License 2.0 (3-clause) License',
+        text: require('licenses/bsd-2.0.txt')
+    },
+    LGPL2_1: {
+        name: 'GNU Lesser General Public License (LGPL) 2.1',
+        text: require('licenses/lgpl-2.1.txt')
+    }    
+};
