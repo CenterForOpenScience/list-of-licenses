df5ec2691b80d590e4ffe7ff17d9b844a759e7f2
Sam Chrisinger <s.chrisinger@gmail.com>
2015-09-28 12:34:43 -0400

    Add BSD3 and LGLP3

--- index.js
+++ index.js
@@ -26,10 +26,17 @@ module.exports = {
         osiApproved: true
     },
     BSD2: {
-        name: 'BSD License 2.0 (3-clause) License',
+        name: 'BSD 2-Clause "Simplified" License',
         text: require('raw!./licenses/bsd-2.0.txt'),
         url: 'http://opensource.org/licenses/BSD-3-Clause',
         logoUrl: require('raw!./img/base64/bsd-2.0.base64'),
+        osiApproved: true,
+        properties: ['year', 'copyrightHolders']
+    },
+    BSD3: {
+        name: 'BSD 3-Clause "New"/"Revised" License',
+        text: require('raw!./licenses/bsd-3.0.txt'),
+        url: 'http://opensource.org/licenses/BSD-3-Clause',
         osiApproved: true,
         properties: ['year', 'copyrightHolders']
     },
@@ -37,6 +44,12 @@ module.exports = {
         name: 'GNU Lesser General Public License (LGPL) 2.1',
         text: require('raw!./licenses/lgpl-2.1.txt'),
         url: 'http://www.gnu.org/licenses/lgpl-2.1.txt',
+        osiApproved: true
+    },
+    LGPL3: {
+        name: 'GNU Lesser General Public License (LGPL) 3.0',
+        text: require('raw!./licenses/lgpl-2.1.txt'),
+        url: 'http://www.gnu.org/licenses/lgpl-3.0.txt',
         osiApproved: true
     },
     CC0: {
@@ -47,31 +60,31 @@ module.exports = {
         osiApproved: false
     },
     CCBY: {
-        name: 'CC By (4.0)',
+        name: 'CC-By Attribution 4.0 International',
         text: require('raw!./licenses/ccby-4.0.txt'),
         url: 'https://creativecommons.org/licenses/by/4.0/legalcode',
         logoUrl: require('raw!./img/base64/ccby-4.0.base64'),
         osiApproved: false
     },
-    Artistic: {
+    Artistic2: {
         name: 'Artistic License 2.0',
         text: require('raw!./licenses/artistic-2.0.txt'),
         url: 'http://www.perlfoundation.org/attachment/legal/artistic-2_0.txt',
         osiApproved: true
     },
-    Eclipse: {
+    Eclipse1: {
         name: 'Eclipse Public License 1.0',
         text: require('raw!./licenses/eclipse-1.0.txt'),
         url: 'http://www.eclipse.org/legal/epl-v10.html',
         osiApproved: true
     },
-    Mozilla: {
+    Mozilla2: {
         name: 'Mozilla Public License 2.0',
         text: require('raw!./licenses/mozilla-2.0.txt'),
         url: 'https://www.mozilla.org/MPL/2.0/',
         osiApproved: true
     },
-    'AFL': {
+    AFL3: {
         name: 'Academic Free License (AFL) 3.0',
         text: require('raw!./licenses/afl-3.0.txt'),
         url: 'http://opensource.org/licenses/AFL-3.0',
