8f2bce71fc97aea386d1a839ca7f30dbae5ba715
Sam Chrisinger <s.chrisinger@gmail.com>
2015-10-22 13:33:17 -0400

    Major cleanup, and just output a JSON file

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,6 @@
 var gulp = require('gulp');
-var gutil = require("gulp-util");
-var webpack = require("webpack");
-var webpackConfig = require("./webpack.config.js");
+var shell = require('gulp-shell');
 
-var fs = require('fs');
-
-gulp.task('encodeImages', function() {
-    fs.readdir('./img/orig/', function(err, files) {
-        if (!err) {
-            for (var i = 0; i < files.length; i++) {
-                var fname = files[i];
-                var fparts = fname.split('.');
-                var fbase = fparts.slice(0, -1).join('.');
-                var fext = fparts.slice(-1);
-
-                var data = fs.readFileSync('./img/orig/' + fname);
-                fs.writeFileSync(
-                    './img/base64/' + fbase + '.base64',
-                    'data:image/' + fext + ';base64, ' +new Buffer(data).toString('base64')
-                );
-            }
-        }
-    });
-});
-
-// create a single instance of the compiler to allow caching
-gulp.task("webpack", function(callback) {
-    // run webpack
-    webpack(webpackConfig, function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
-        }));
-        callback();
-    });
+gulp.task("default", function(callback) {
+    shell('node index.js');
 });
--- index.js
+++ index.js
@@ -1,93 +1,93 @@
-module.exports = {
+var fs = require('fs');
+
+var lol = {
     MIT: {
         name: 'MIT License',
-        text: require('raw!./licenses/mit.txt'),
+        text: fs.readFileSync('./licenses/mit.txt').toString(),
         url: 'http://opensource.org/licenses/MIT',
         osiApproved: true,
         properties: ['year', 'copyrightHolders']
     },
     GPL2: {
         name: 'GNU General Public License (GPL) 2.0',
-        text: require('raw!./licenses/gpl-2.0.txt'),
+        text: fs.readFileSync('./licenses/gpl-2.0.txt').toString(),
         url: 'http://www.gnu.org/licenses/gpl-2.0.txt',
         osiApproved: true
     },
     GPL3: {
         name: 'GNU General Public License (GPL) 3.0',
-        text: require('raw!./licenses/gpl-3.0.txt'),
+        text: fs.readFileSync('./licenses/gpl-3.0.txt').toString(),
         url: 'http://www.gnu.org/licenses/gpl-3.0.txt',
-        logoUrl: require('raw!./img/base64/gpl-3.0.base64'),
         osiApproved: true
     },
     Apache2: {
         name: 'Apache License 2.0',
-        text: require('raw!./licenses/apache-2.0.txt'),
+        text: fs.readFileSync('./licenses/apache-2.0.txt').toString(),
         url: 'http://www.apache.org/licenses/LICENSE-2.0.html',
         osiApproved: true
     },
     BSD2: {
         name: 'BSD 2-Clause "Simplified" License',
-        text: require('raw!./licenses/bsd-2.0.txt'),
+        text: fs.readFileSync('./licenses/bsd-2.0.txt').toString(),
         url: 'http://opensource.org/licenses/BSD-3-Clause',
-        logoUrl: require('raw!./img/base64/bsd-2.0.base64'),
         osiApproved: true,
         properties: ['year', 'copyrightHolders']
     },
     BSD3: {
         name: 'BSD 3-Clause "New"/"Revised" License',
-        text: require('raw!./licenses/bsd-3.0.txt'),
+        text: fs.readFileSync('./licenses/bsd-3.0.txt').toString(),
         url: 'http://opensource.org/licenses/BSD-3-Clause',
         osiApproved: true,
         properties: ['year', 'copyrightHolders']
     },
     LGPL2_1: {
         name: 'GNU Lesser General Public License (LGPL) 2.1',
-        text: require('raw!./licenses/lgpl-2.1.txt'),
+        text: fs.readFileSync('./licenses/lgpl-2.1.txt').toString(),
         url: 'http://www.gnu.org/licenses/lgpl-2.1.txt',
         osiApproved: true
     },
     LGPL3: {
         name: 'GNU Lesser General Public License (LGPL) 3.0',
-        text: require('raw!./licenses/lgpl-2.1.txt'),
+        text: fs.readFileSync('./licenses/lgpl-2.1.txt').toString(),
         url: 'http://www.gnu.org/licenses/lgpl-3.0.txt',
         osiApproved: true
     },
     CC0: {
         name: 'CC0 1.0 Universal',
-        text: require('raw!./licenses/cc0-1.0.txt'),
+        text: fs.readFileSync('./licenses/cc0-1.0.txt').toString(),
         url: 'https://creativecommons.org/publicdomain/zero/1.0/legalcode',
-        logoUrl: require('raw!./img/base64/cc0-1.0.base64'),
         osiApproved: false
     },
     CCBY: {
         name: 'CC-By Attribution 4.0 International',
-        text: require('raw!./licenses/ccby-4.0.txt'),
+        text: fs.readFileSync('./licenses/ccby-4.0.txt').toString(),
         url: 'https://creativecommons.org/licenses/by/4.0/legalcode',
-        logoUrl: require('raw!./img/base64/ccby-4.0.base64'),
         osiApproved: false
     },
     Artistic2: {
         name: 'Artistic License 2.0',
-        text: require('raw!./licenses/artistic-2.0.txt'),
+        text: fs.readFileSync('./licenses/artistic-2.0.txt').toString(),
         url: 'http://www.perlfoundation.org/attachment/legal/artistic-2_0.txt',
         osiApproved: true
     },
     Eclipse1: {
         name: 'Eclipse Public License 1.0',
-        text: require('raw!./licenses/eclipse-1.0.txt'),
+        text: fs.readFileSync('./licenses/eclipse-1.0.txt').toString(),
         url: 'http://www.eclipse.org/legal/epl-v10.html',
         osiApproved: true
     },
     Mozilla2: {
         name: 'Mozilla Public License 2.0',
-        text: require('raw!./licenses/mozilla-2.0.txt'),
+        text: fs.readFileSync('./licenses/mozilla-2.0.txt').toString(),
         url: 'https://www.mozilla.org/MPL/2.0/',
         osiApproved: true
     },
     AFL3: {
         name: 'Academic Free License (AFL) 3.0',
-        text: require('raw!./licenses/afl-3.0.txt'),
+        text: fs.readFileSync('./licenses/afl-3.0.txt').toString(),
         url: 'http://opensource.org/licenses/AFL-3.0',
         osiApproved: true
     }
 };
+
+fs.writeFile('dist/list-of-licenses.json', JSON.stringify(lol));
